a9aa3bc9607d181d8382998e3bcae29d7e68c5f4
Michelle Ranly <meranly8@gmail.com>
2020-12-11 05:31:31 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,10 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
   
+  get 'recipes/new' do
+    erb :new
+  end
+    
   get '/recipes' do
     @recipes = Recipe.all
     erb :index
